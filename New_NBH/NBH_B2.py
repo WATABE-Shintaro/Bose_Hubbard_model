c61eb43bdd0cb3c2d84d2f9419be21d05eff4648
watabeDD <watabe_shintaro@icloud.com>
2022-07-01 09:17:09 +0900

    DataLoarder

--- New_NBH/NBH_B2.py
+++ New_NBH/NBH_B2.py
@@ -11,22 +11,26 @@ import torch.nn as nn
 import torch.nn.functional as func
 import torch.optim as optim
 import torch.cuda
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 
 # 変更用オプション
 #################################################################################
 # 　d_はデフォルトの略、コマンドからの時は変更できる
-d_EPOCH = 5000
+d_EPOCH = 200
+d_SAMPLE_NUM = 99000
+d_BATCH_SIZE = 9900
 # 格子点数および粒子数
-d_LATTICE = 7
-d_PARTICLE = 5
+d_LATTICE = 11
+d_PARTICLE = 9
 # パラメータ
-d_U = 4
+d_U = 2
 d_J = 1
 # output アウトプットするデータの名前
 d_OUTPUT_FILE_NAME = "AAA"
 # GPUを使う場合'cuda' cpu なら'cpu'
 d_GPU = 'cuda'
-d_SAMPLE_NUM = 35000
+
 ################################################################################
 
 # 学習率
@@ -64,23 +68,40 @@ class MyLoss(nn.Module):
         self.i_vector_array = torch.from_numpy(self.i_vector_array)
         self.i_vector_array = self.i_vector_array.to(DEVICE)
 
-    def forward(self, n_vector_array, cn_array, cn_tensor_1, cn_tensor_2):
+    def forward(self, n_vector_array, cn_array, cn_tensor_1, cn_tensor_2, n_correction_array):
         cn_array = cn_array[:, 0] * torch.exp(cn_array[:, 1] * 1j)
         cn_tensor_1 = cn_tensor_1[:, :, 0] * torch.exp(cn_tensor_1[:, :, 1] * 1j)
         cn_tensor_2 = cn_tensor_2[:, :, 0] * torch.exp(cn_tensor_2[:, :, 1] * 1j)
         return (-J * torch.sum(torch.sqrt(n_vector_array[:, 0:LATTICE - 1] * (n_vector_array[:, 1:LATTICE] + 1)) *
-                               torch.t(torch.conj(cn_tensor_1) * cn_array)
+                               torch.conj(cn_tensor_1) * torch.reshape(cn_array *
+                                                                       n_correction_array, (-1, 1))
                                + torch.sqrt((n_vector_array[:, 0:LATTICE - 1] + 1) * n_vector_array[:, 1:LATTICE]) *
-                               torch.t(torch.conj(cn_tensor_2) * cn_array))
-                + J * torch.sum(
-                    self.i_vector_array ** 2 * n_vector_array * torch.reshape(torch.abs(cn_array) ** 2, (-1, 1)))
-                - J * (LATTICE - 1) * torch.sum(
-                    self.i_vector_array * n_vector_array * torch.reshape(torch.abs(cn_array) ** 2, (-1, 1)))
-                + ((LATTICE - 1) ** 2 * J / 4 - U / 2) * torch.sum(
-                    n_vector_array * torch.reshape(torch.abs(cn_array) ** 2, (-1, 1)))
-                + U / 2 * torch.sum(
-                    n_vector_array ** 2 * torch.reshape(torch.abs(cn_array) ** 2, (-1, 1)))) / torch.sum(
-            torch.abs(cn_array) ** 2)
+                               torch.conj(cn_tensor_2) * torch.reshape(cn_array *
+                                                                       n_correction_array, (-1, 1)))
+                + J * torch.sum((self.i_vector_array ** 2 - (LATTICE - 1) * self.i_vector_array + (
+                        LATTICE - 1) ** 2 / 4) * n_vector_array * torch.reshape(
+                    torch.abs(cn_array) ** 2 * n_correction_array,
+                    (-1, 1)))
+                + U / 2 * torch.sum((n_vector_array ** 2 - n_vector_array) * torch.reshape(
+                    torch.abs(cn_array) ** 2 * n_correction_array,
+                    (-1, 1)))) / torch.sum(torch.abs(cn_array) ** 2 * n_correction_array)
+
+
+class MyDataset(Dataset):
+    def __init__(self, n_vector_array, n_vector_tensor1, n_vector_tensor2, n_correction_array):
+        super().__init__()
+
+        self.n_vector_array = n_vector_array
+        self.n_vector_tensor1 = n_vector_tensor1
+        self.n_vector_tensor2 = n_vector_tensor2
+        self.n_correction_array = n_correction_array
+
+    def __len__(self):
+        return len(self.n_vector_array)
+
+    def __getitem__(self, idx):
+        return self.n_vector_array[idx], self.n_vector_tensor1[idx], self.n_vector_tensor2[idx], \
+               self.n_correction_array[idx]
 
 
 def est_particle(n_vector_array, i, cn_array):
@@ -102,20 +123,37 @@ def montecarlo(sample_num):
     return n_vector_array
 
 
+def montecarlo_correction(n_vector_array, sample_num):
+    n_correction_array = np.ones(sample_num)
+    for i in range(sample_num):
+        correction = 1
+        for j in range(1, LATTICE - 1):
+            if n_vector_array[i, j] == 0:
+                correction += 1
+            else:
+                n_correction_array[i] *= math.factorial(correction)
+                correction = 1
+        n_correction_array[i] *= math.factorial(correction)
+    n_correction_array = torch.from_numpy(n_correction_array).float()
+    n_correction_array = n_correction_array.to(DEVICE)
+    return n_correction_array
+
+
 def make_sample(n_vector_array, sample_num):
-    n_vector_tensor_1 = np.zeros([LATTICE - 1, sample_num, LATTICE])
+    n_vector_tensor_1 = np.zeros([sample_num, LATTICE - 1, LATTICE])
     n_vector_tensor_1 = torch.from_numpy(n_vector_tensor_1).float()
     n_vector_tensor_1 = n_vector_tensor_1.to(DEVICE)
-    n_vector_tensor_2 = np.zeros([LATTICE - 1, sample_num, LATTICE])
+    n_vector_tensor_2 = np.zeros([sample_num, LATTICE - 1, LATTICE])
     n_vector_tensor_2 = torch.from_numpy(n_vector_tensor_2).float()
     n_vector_tensor_2 = n_vector_tensor_2.to(DEVICE)
     for i in range(LATTICE - 1):
-        n_vector_tensor_1[i] = copy.deepcopy(n_vector_array)
-        n_vector_tensor_1[i, :, i] -= 1
-        n_vector_tensor_1[i, :, i + 1] += 1
-        n_vector_tensor_2[i] = copy.deepcopy(n_vector_array)
-        n_vector_tensor_2[i, :, i] += 1
-        n_vector_tensor_2[i, :, i + 1] -= 1
+        n_vector_tensor_1[:, i] = copy.deepcopy(n_vector_array)
+        n_vector_tensor_1[:, i, i] -= 1
+        n_vector_tensor_1[:, i, i + 1] += 1
+        n_vector_tensor_2[:, i] = copy.deepcopy(n_vector_array)
+        n_vector_tensor_2[:, i, i] += 1
+        n_vector_tensor_2[:, i, i + 1] -= 1
+
     return n_vector_tensor_1, n_vector_tensor_2
 
 
@@ -142,7 +180,24 @@ def learning():
         f.write("STEP:" + str(LR_STEP) + "\n ")
         f.write("Gamma:" + str(LR_GAMMA) + "\n ")
         f.write("Momentum:" + str(MOMENTUM) + "\n ")
+        f.write("sample:" + str(SAMPLE_NUM) + "\n ")
+        f.write("Batch:" + str(BATCH_SIZE) + "\n ")
+        f.write("U:" + str(U) + "\n ")
+        f.write("J:" + str(J) + "\n ")
     print("データロード開始")
+
+    train_n_vector_array = montecarlo(SAMPLE_NUM)
+    train_n_vector_tensor_1, train_n_vector_tensor_2 = make_sample(train_n_vector_array, SAMPLE_NUM)
+    train_n_correction_array = montecarlo_correction(train_n_vector_array, SAMPLE_NUM)
+    test_n_vector_array = montecarlo(BATCH_SIZE)
+    test_n_vector_tensor_1, test_n_vector_tensor_2 = make_sample(test_n_vector_array, BATCH_SIZE)
+    test_n_correction_array = montecarlo_correction(test_n_vector_array, BATCH_SIZE)
+    train_dataset = MyDataset(train_n_vector_array, train_n_vector_tensor_1, train_n_vector_tensor_2,
+                              train_n_correction_array)
+    test_dataset = MyDataset(test_n_vector_array, test_n_vector_tensor_1, test_n_vector_tensor_2,
+                             test_n_correction_array)
+    train_dataloader = DataLoader(train_dataset, BATCH_SIZE, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, BATCH_SIZE, shuffle=True)
 
     # ニューラルネットワークを実体化
     my_net: nn.Module = MyModel()
@@ -151,61 +206,62 @@ def learning():
     optimizer = optim.SGD(params=my_net.parameters(), lr=LR, momentum=MOMENTUM)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=LR_STEP, gamma=LR_GAMMA)
 
-    criterion = MyLoss(SAMPLE_NUM)
+    criterion = MyLoss(BATCH_SIZE)
 
     # 学習結果の保存用
     history = {'train_loss': [], 'test_loss': [], 'now_time': [], }
 
     # 学習開始。エポック数だけ学習を繰り返す。
+    print("学習開始")
     for e in range(EPOCH):
         # 学習を行う。
-        train_n_vector_array = montecarlo(SAMPLE_NUM)
-        train_n_vector_tensor_1, train_n_vector_tensor_2 = make_sample(train_n_vector_array, SAMPLE_NUM)
+
         my_net.train(True)
-        optimizer.zero_grad()
-        cn_array = my_net(train_n_vector_array)
-        cn_tensor_1 = my_net(train_n_vector_tensor_1)
-        cn_tensor_2 = my_net(train_n_vector_tensor_2)
-        energy = criterion(train_n_vector_array, cn_array, cn_tensor_1, cn_tensor_2)
-        energy.backward()
-        optimizer.step()
+        for tr_n_v_a, tr_n_v_t_1, tr_n_v_t_2, tr_n_c_a in train_dataloader:
+            optimizer.zero_grad()
+            cn_array = my_net(tr_n_v_a)
+            cn_tensor_1 = my_net(tr_n_v_t_1)
+            cn_tensor_2 = my_net(tr_n_v_t_2)
+            energy = criterion(tr_n_v_a, cn_array, cn_tensor_1, cn_tensor_2, tr_n_c_a)
+            energy.backward()
+            optimizer.step()
         train_loss = energy.item()
         scheduler.step()
         # テストを行う。
-        if e % 10 == 0:
-            history['train_loss'].append(train_loss)
-            my_net.eval()
-            with torch.no_grad():
-                test_n_vector_array = montecarlo(SAMPLE_NUM)
-                test_n_vector_tensor_1, test_n_vector_tensor_2 = make_sample(test_n_vector_array, SAMPLE_NUM)
-                cn_array = my_net(test_n_vector_array)
-                cn_tensor_1 = my_net(test_n_vector_tensor_1)
-                cn_tensor_2 = my_net(test_n_vector_tensor_2)
-                energy = criterion(test_n_vector_array, cn_array, cn_tensor_1, cn_tensor_2)
-                test_loss = energy.item()
-            history['test_loss'].append(test_loss)
-            # 経過を記録して、表示。
-            now_time = datetime.datetime.now()
-            history['now_time'].append(now_time.strftime('%Y/%m/%d %H:%M:%S'))
-            print('Train Epoch: {}/{} \t TrainLoss: {:.6f} \t TestLoss: {:.6f} \t time: {} \t lr:{}'
-                  .format(e + 1, EPOCH, train_loss, test_loss, now_time.strftime('%Y/%m/%d %H:%M:%S'),
-                          scheduler.get_last_lr()[0]))
+
+        history['train_loss'].append(train_loss)
+        my_net.eval()
+        with torch.no_grad():
+            for te_n_v_a, te_n_v_t_1, te_n_v_t_2, te_n_c_a in test_dataloader:
+                cn_array = my_net(te_n_v_a)
+                cn_tensor_1 = my_net(te_n_v_t_1)
+                cn_tensor_2 = my_net(te_n_v_t_2)
+                energy = criterion(te_n_v_a, cn_array, cn_tensor_1, cn_tensor_2, te_n_c_a)
+        test_loss = energy.item()
+        history['test_loss'].append(test_loss)
+        # 経過を記録して、表示。
+        now_time = datetime.datetime.now()
+        history['now_time'].append(now_time.strftime('%Y/%m/%d %H:%M:%S'))
+        print('Train Epoch: {}/{} \t TrainLoss: {:.6f} \t TestLoss: {:.6f} \t time: {} \t lr:{}'
+              .format(e + 1, EPOCH, train_loss, test_loss, now_time.strftime('%Y/%m/%d %H:%M:%S'),
+                      scheduler.get_last_lr()[0]))
 
     print("学習終了")
     # 予測を行う。
     my_net.eval()
     n_vector_result = np.zeros([LATTICE])
     with torch.no_grad():
-        criterion = MyLoss(SAMPLE_NUM * 10)
-        est_n_vector_array = montecarlo(SAMPLE_NUM * 10)
-        est_n_vector_tensor_1, est_n_vector_tensor_2 = make_sample(est_n_vector_array, SAMPLE_NUM * 10)
+        criterion = MyLoss(SAMPLE_NUM)
+        est_n_vector_array = montecarlo(SAMPLE_NUM)
+        est_n_vector_tensor_1, est_n_vector_tensor_2 = make_sample(est_n_vector_array, SAMPLE_NUM)
+        est_n_correction_array = montecarlo_correction(est_n_vector_array, SAMPLE_NUM)
         cn_array = my_net(est_n_vector_array)
         cn_tensor_1 = my_net(est_n_vector_tensor_1)
         cn_tensor_2 = my_net(est_n_vector_tensor_2)
-        energy = criterion(est_n_vector_array, cn_array, cn_tensor_1, cn_tensor_2)
+        energy = criterion(est_n_vector_array, cn_array, cn_tensor_1, cn_tensor_2, est_n_correction_array)
         for i in range(LATTICE):
             n_vector_result[i] = est_particle(est_n_vector_array, i, cn_array)
-            est_loss = energy.item()
+        est_loss = energy.item()
 
     # 結果をセーブする。
     if not os.path.exists(args.out + "/" + OUTPUT_NAME):
@@ -226,10 +282,43 @@ def learning():
     return str(est_loss), str(n_vector_result)
 
 
+"""
+def forward(n_vector_array, cn_array, cn_tensor_1, cn_tensor_2, n_correction_array):
+    i_vector_array = [np.arange(LATTICE) for i in range(SAMPLE_NUM * 10)]
+    i_vector_array = np.array(i_vector_array)
+    i_vector_array = torch.from_numpy(i_vector_array)
+    i_vector_array = i_vector_array.to(DEVICE)
+    cn_array = cn_array[:, 0] * torch.exp(cn_array[:, 1] * 1j)
+    cn_tensor_1 = cn_tensor_1[:, :, 0] * torch.exp(cn_tensor_1[:, :, 1] * 1j)
+    cn_tensor_2 = cn_tensor_2[:, :, 0] * torch.exp(cn_tensor_2[:, :, 1] * 1j)
+    d = torch.sum(torch.abs(cn_array) ** 2)
+    q = torch.reshape(n_correction_array, (-1, 1))
+    a = -J * torch.sum(torch.sqrt(n_vector_array[:, 0:LATTICE - 1] * (n_vector_array[:, 1:LATTICE] + 1)) *
+                       torch.t(torch.conj(cn_tensor_1) * cn_array)
+                       + torch.sqrt((n_vector_array[:, 0:LATTICE - 1] + 1) * n_vector_array[:, 1:LATTICE]) *
+                       torch.t(torch.conj(cn_tensor_2) * cn_array) * torch.reshape(n_correction_array,
+                                                                                   (-1, 1))) / d
+    c0 = n_vector_array ** 2
+    c1 = n_vector_array
+    b = J * torch.sum(
+        (i_vector_array ** 2 - (LATTICE - 1) * i_vector_array + (
+                LATTICE - 1) ** 2 / 4) * n_vector_array * torch.reshape(torch.abs(cn_array) ** 2,
+                                                                        (-1, 1)) * torch.reshape(n_correction_array,
+                                                                                                 (-1, 1))) / d
+    c = U / 2 * torch.sum(
+        (n_vector_array ** 2 - n_vector_array) * torch.reshape(torch.abs(cn_array) ** 2, (-1, 1)) * torch.reshape(
+            n_correction_array,
+            (-1, 1))) / d
+
+    return a + b + c
+"""
+
 if __name__ == '__main__':
     # コマンドラインからプログラムを動かす時のオプションを実装
     parser = argparse.ArgumentParser(description='Pytorch' + __file__)
     parser.add_argument('--epoch', '-e', type=int, default=d_EPOCH,
+                        help='Number of sweeps over the dataset to train')
+    parser.add_argument('--batch', '-b', type=int, default=d_BATCH_SIZE,
                         help='Number of sweeps over the dataset to train')
     parser.add_argument('--gpu', '-g', type=str, default=d_GPU,
                         help='if you want to use GPU, select cuda. cpu for cpu')
@@ -248,6 +337,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     EPOCH = args.epoch
+    BATCH_SIZE = args.batch
     GPU = args.gpu
     LATTICE = args.M
     PARTICLE = args.N
@@ -267,17 +357,20 @@ if __name__ == '__main__':
     ggg = optim.SGD(params=aaa.parameters(), lr=LR, momentum=MOMENTUM)
     start = time.time()
     bb1 = montecarlo(SAMPLE_NUM)
-    print("metro:{0}".format(time.time() - start) + "[sec]")
+    print("monte:{0}".format(time.time() - start) + "[sec]")
     start = time.time()
     bb2, bb3 = make_sample(bb1, SAMPLE_NUM)
     print("make:{0}".format(time.time() - start) + "[sec]")
+    start = time.time()
+    bbb = montecarlo_correction(bb1, SAMPLE_NUM)
+    print("corr:{0}".format(time.time() - start) + "[sec]")
     start = time.time()
     cc1 = aaa(bb1)
     cc2 = aaa(bb2)
     cc3 = aaa(bb3)
     print("net:{0}".format(time.time() - start) + "[sec]")
     start = time.time()
-    fff = ddd(bb1, cc1, cc2, cc3)
+    fff = ddd(bb1, cc1, cc2, cc3, bbb)
     print("loss:{0}".format(time.time() - start) + "[sec]")
     start = time.time()
     fff.backward()
@@ -286,4 +379,4 @@ if __name__ == '__main__':
     ggg.step()
     print("optim:{0}".format(time.time() - start) + "[sec]")
     print(fff)
-    """
+"""
