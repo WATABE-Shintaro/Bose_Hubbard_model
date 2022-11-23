cfa4dd539b68616c4aeb96775b16c8257fc7dd5e
WATABEshintaro <watabe_shintaro@icloud.com>
2022-11-23 14:33:29 +0900

    aaa

--- New_NBH/NBH_B3.py
+++ New_NBH/NBH_B3.py
@@ -18,31 +18,31 @@ from torch.utils.data import DataLoader
 # 変更用オプション
 #################################################################################
 # 　d_はデフォルトの略、コマンドからの時は変更できる
-d_EPOCH = 200
-d_SAMPLE_NUM = 20480
-d_BATCH_SIZE = 1024
+d_EPOCH = 10
+d_SAMPLE_NUM = 1024000
+d_BATCH_SIZE = 102400
 # 格子点数および粒子数
-d_LATTICE = 11
-d_PARTICLE = 9
+d_LATTICE = 7
+d_PARTICLE = 5
 # パラメータ
 d_U = 2
 d_J = 1
 # output アウトプットするデータの名前
-d_OUTPUT_FILE_NAME = "AAA"
+d_OUTPUT_FILE_NAME = "BBB"
 # GPUを使う場合'cuda' cpu なら'cpu'
 d_GPU = 'cuda'
-
+net_num1 = 40
 ################################################################################
 
 # 学習率
 LR = 0.06
-LR_STEP = 50
+LR_STEP = 2
 LR_GAMMA = 0.6
 MOMENTUM = 0.95
 # 定数
 MEMO_NAME = "memo.txt"  # 条件記録用
 
-net_num = 20
+
 OUTPUT_NAME = "RESULT"
 
 PI = 3.14
@@ -52,8 +52,8 @@ class MyModel(nn.Module):
     def __init__(self):
         super().__init__()
         # 全結合ネットワーク
-        self.l1 = nn.Linear(LATTICE, net_num)
-        self.l2 = nn.Linear(net_num, 2)
+        self.l1 = nn.Linear(LATTICE, net_num1)
+        self.l2 = nn.Linear(net_num1, 2)
 
     def forward(self, x):
         # reluを用いる。
@@ -247,6 +247,7 @@ def learning():
         for i in range(LATTICE):
             n_vector_result[i] = est_particle(est_n_vector_array, i, cn_array)
         est_loss = energy.item()
+        #aaa = forward(est_n_vector_array, cn_array, cn_tensor_1, cn_tensor_2)
 
     # 結果をセーブする。
     if not os.path.exists(args.out + "/" + OUTPUT_NAME):
@@ -268,32 +269,27 @@ def learning():
 
 
 """
-def forward(n_vector_array, cn_array, cn_tensor_1, cn_tensor_2, n_correction_array):
-    i_vector_array = [np.arange(LATTICE) for i in range(SAMPLE_NUM * 10)]
+def forward(n_vector_array, cn_array, cn_tensor_1, cn_tensor_2):
+    i_vector_array = [np.arange(LATTICE) for i in range(SAMPLE_NUM)]
     i_vector_array = np.array(i_vector_array)
     i_vector_array = torch.from_numpy(i_vector_array)
     i_vector_array = i_vector_array.to(DEVICE)
-    cn_array = cn_array[:, 0] * torch.exp(cn_array[:, 1] * 1j)
-    cn_tensor_1 = cn_tensor_1[:, :, 0] * torch.exp(cn_tensor_1[:, :, 1] * 1j)
-    cn_tensor_2 = cn_tensor_2[:, :, 0] * torch.exp(cn_tensor_2[:, :, 1] * 1j)
-    d = torch.sum(torch.abs(cn_array) ** 2)
-    q = torch.reshape(n_correction_array, (-1, 1))
+    cn_array = cn_array[:, 0] * torch.exp(cn_array[:, 1] * PI * 1j)
+    cn_tensor_1 = cn_tensor_1[:, :, 0] * torch.exp(cn_tensor_1[:, :, 1] * PI * 1j)
+    cn_tensor_2 = cn_tensor_2[:, :, 0] * torch.exp(cn_tensor_2[:, :, 1] * PI * 1j)
+    e = torch.abs(cn_array) ** 2
+    d = torch.sum(e)
     a = -J * torch.sum(torch.sqrt(n_vector_array[:, 0:LATTICE - 1] * (n_vector_array[:, 1:LATTICE] + 1)) *
-                       torch.t(torch.conj(cn_tensor_1) * cn_array)
-                       + torch.sqrt((n_vector_array[:, 0:LATTICE - 1] + 1) * n_vector_array[:, 1:LATTICE]) *
-                       torch.t(torch.conj(cn_tensor_2) * cn_array) * torch.reshape(n_correction_array,
-                                                                                   (-1, 1))) / d
-    c0 = n_vector_array ** 2
-    c1 = n_vector_array
-    b = J * torch.sum(
-        (i_vector_array ** 2 - (LATTICE - 1) * i_vector_array + (
-                LATTICE - 1) ** 2 / 4) * n_vector_array * torch.reshape(torch.abs(cn_array) ** 2,
-                                                                        (-1, 1)) * torch.reshape(n_correction_array,
-                                                                                                 (-1, 1))) / d
-    c = U / 2 * torch.sum(
-        (n_vector_array ** 2 - n_vector_array) * torch.reshape(torch.abs(cn_array) ** 2, (-1, 1)) * torch.reshape(
-            n_correction_array,
-            (-1, 1))) / d
+                               torch.conj(cn_tensor_1) * torch.reshape(cn_array, (-1, 1))
+                               + torch.sqrt((n_vector_array[:, 0:LATTICE - 1] + 1) * n_vector_array[:, 1:LATTICE]) *
+                               torch.conj(cn_tensor_2) * torch.reshape(cn_array, (-1, 1))) / d
+    b = J * torch.sum((i_vector_array ** 2 - (LATTICE - 1) * i_vector_array + (
+                        LATTICE - 1) ** 2 / 4) * n_vector_array * torch.reshape(
+                    torch.abs(cn_array) ** 2 ,(-1, 1))) / d
+    b2 = J * torch.sum(
+        (i_vector_array  - (LATTICE - 1) / 2) ** 2 * n_vector_array * torch.reshape(torch.abs(cn_array) ** 2,(-1, 1)) ) / d                                                                    
+    c =  U / 2 * torch.sum((n_vector_array ** 2 - n_vector_array) * torch.reshape(
+                    torch.abs(cn_array) ** 2 ,(-1, 1))) / d
 
     return a + b + c
 """
